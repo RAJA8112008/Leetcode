9f36b0b2d44a1df14e7d2fffc3ed1a6588bbb9f2
Raja Kumar <kraj9380286@gmail.com>
2026-09-05 19:16:23 +0530

    Time: 5 ms (42.76%), Space: 23.6 MB (85.76%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        //first store freq 
-        vector<int>ans;
-        int n=nums.size();
-        unordered_map<int,int>mp;
+        int count1=0;
+        int maj1=0;
+        int count2=0;
+        int maj2=0;
         for(int i=0;i<nums.size();i++){
-            mp[nums[i]]++;
-        }
-        //search if freq is more then n/3 
-        for(auto val:mp){
-            if(val.second>n/3){
-                ans.push_back(val.first);
+            if(nums[i]==maj1){
+                count1++;
+            }else if(nums[i]==maj2){
+                count2++;
+            }else if(count1==0){
+                count1++;
+                maj1=nums[i];
+            }else if(count2==0){
+                count2++;
+                maj2=nums[i];
+            }else{
+                count1--;
+                count2--;
             }
+        }
+        vector<int>ans;
+        int c1=0;
+        int c2=0;
+        for(auto val:nums){
+            if(val==maj1){
+                c1++;
+            }else if(val==maj2){
+                c2++;
+            }
+        }
+        if(c1>nums.size()/3){
+            ans.push_back(maj1);
+        }
+         if(c2>nums.size()/3){
+            ans.push_back(maj2);
         }
         return ans;
     }

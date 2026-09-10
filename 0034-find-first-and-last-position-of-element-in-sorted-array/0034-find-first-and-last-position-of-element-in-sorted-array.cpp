2652a068e35513f108f113707597b01797a8436c
Raja Kumar <kraj9380286@gmail.com>
2026-09-10 19:04:41 +0530

    Time: 0 ms (100%), Space: 17.5 MB (88.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
-int firstOccurence(vector<int>&nums,int target){
+void upperbound(vector<int>&nums,int target,int& first){
+    int n=nums.size();
     int st=0;
-    int ed=nums.size()-1;
-    int ans1=-1;
+    int ed=n-1;
     while(st<=ed){
         int mid=st+(ed-st)/2;
-        if(nums[mid]==target){
-            ans1=mid;
-            ed=mid-1;
-        }else if(nums[mid]<target){
-            st=mid+1;
-        }else{
-            ed=mid-1;
-        }
-    }
-    return ans1;
-}
-
-int secondOccurence(vector<int>&nums,int target){
-    int st=0;
-    int ed=nums.size()-1;
-    int ans2=-1;
-    while(st<=ed){
-        int mid=st+(ed-st)/2;
-        if(nums[mid]==target){
-            ans2=mid;
+        if(nums[mid]<=target){
+            if(nums[mid]==target){
+                first=mid;
+            }
+            
            st=mid+1;
-        }else if(nums[mid]<target){
-            st=mid+1;
         }else{
             ed=mid-1;
         }
     }
-    return ans2;
+}
+//secoind occurence 
+void lowerbound(vector<int>&nums,int target,int& second){
+    int n=nums.size();
+    int st=0;
+    int ed=n-1;
+    while(st<=ed){
+        int mid=st+(ed-st)/2;
+        if(nums[mid]>=target){
+            if(nums[mid]==target){
+                second=mid;
+            }
+            
+            ed=mid-1;
+        }else{
+             st=mid+1;
+        }
+    }
 }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first=firstOccurence(nums,target);
-        int second=secondOccurence(nums,target);
+        //first find lower bound 
+        int first=-1;
+        int second=-1;
+        lowerbound(nums,target,first);
+        upperbound(nums,target,second);
         return {first,second};
     }
 };

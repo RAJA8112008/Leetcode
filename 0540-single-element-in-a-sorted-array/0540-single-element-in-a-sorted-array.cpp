4d2613ce91aead7aefefbdbd318ead2a15eed9c2
Raja Kumar <kraj9380286@gmail.com>
2026-09-11 23:28:25 +0530

    Time: 0 ms (100%), Space: 26.1 MB (71.16%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -5,13 +5,11 @@ public:
         int ed=nums.size()-1;
         while(st<ed){
             int mid=st+(ed-st)/2;
-            if(mid%2==1){
-                mid=mid-1;
-            }
+            if(mid %2==1)mid--; //becoue each val is in twice so make sure it is even
             if(nums[mid]==nums[mid+1]){
                 st=mid+2;
             }else{
-               ed=mid;
+                ed=mid;
             }
         }
         return nums[ed];
